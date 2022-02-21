3903956230370c7eb3edf48cfeff5e8d2fc8b45f
xiaoxuan <yuxiaoxuan@guestin.cn>
2022-02-21 19:36:13 +0800

    首页布局

--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -1,6 +1,37 @@
 <template>
   <div class="home">
-    
+    <el-card class="box-card">
+      <template #header>
+        <div class="card-header">
+          <span>Electron</span>
+        </div>
+      </template>
+      <div></div>
+    </el-card>
+    <el-card class="box-card">
+      <template #header>
+        <div class="card-header">
+          <span>vite</span>
+        </div>
+      </template>
+      <div></div>
+    </el-card>
+    <el-card class="box-card">
+      <template #header>
+        <div class="card-header">
+          <span>Vue3</span>
+        </div>
+      </template>
+      <div></div>
+    </el-card>
+    <el-card class="box-card">
+      <template #header>
+        <div class="card-header">
+          <span>Element-plus</span>
+        </div>
+      </template>
+      <div></div>
+    </el-card>
   </div>
 </template>
 
@@ -21,12 +52,30 @@ export default defineComponent({
   width: 100%;
   height: 100%;
   position: relative;
-}
-.btnReadDoorCard {
-  position: absolute;
-  right: 2px;
-  top: 0;
-  z-index: 10;
-  width: 220px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content:space-between;
+  justify-content: center;
+  align-content: flex-start;
+  .card-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .box-card {
+    width: 45%;
+    height: 480px;
+    margin: 10px;
+
+  }
 }
 </style>
