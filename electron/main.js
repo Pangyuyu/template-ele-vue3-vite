60faae7cb3945db57c83e28f280dc60128418b16
pc-xuan\xuan <1633603801@qq.com>
2022-04-29 17:48:07 +0800

    主进程日志简单封装

--- electron/main.js
+++ electron/main.js
@@ -1,27 +1,28 @@
 // electron/electron.js
 const { app } = require('electron');
-const {AppStart} =require("./AppStart")
-const {AppMenu}=require("./AppMenu")
+const { AppStart } = require("./AppStart")
+const { AppMenu } = require("./AppMenu")
 const isDev = process.env.IS_DEV == "true" ? true : false;
 const shouldQuit = app.requestSingleInstanceLock() //单实例
-const appStart=new AppStart()
-const appMenu=new AppMenu()
+const appStart = new AppStart()
+const appMenu = new AppMenu()
+let mainWin = null //窗口
 /*在开发模式下，应父进程（parent process）的要求完全退出。 */
 
 if (isDev) {
   if (process.platform === "win32") {
-      process.throwDeprecation=true  //用于控制是否将弃用警告作为异常抛出。将此设置为true会引发弃用错误。使用此属性代替
-      process.traceDeprecation=true //用于控制是否打印弃警告的堆栈跟踪到stderr。将此设置为true将打印废弃警告的堆栈跟踪
-      process.traceProcessWarnings=true //用于控制是否打印进程警告及其堆栈跟踪到stderr。将此设置为true将会为进程警告（包括弃用）打印堆栈跟踪
-      process.on("message", (data) => {
-          if (data === "graceful-exit") {
-              app.quit()
-          }
-      })
+    process.throwDeprecation = true  //用于控制是否将弃用警告作为异常抛出。将此设置为true会引发弃用错误。使用此属性代替
+    process.traceDeprecation = true //用于控制是否打印弃警告的堆栈跟踪到stderr。将此设置为true将打印废弃警告的堆栈跟踪
+    process.traceProcessWarnings = true //用于控制是否打印进程警告及其堆栈跟踪到stderr。将此设置为true将会为进程警告（包括弃用）打印堆栈跟踪
+    process.on("message", (data) => {
+      if (data === "graceful-exit") {
+        app.quit()
+      }
+    })
   } else {
-      process.on("SIGTERM", () => {
-          app.quit()
-      })
+    process.on("SIGTERM", () => {
+      app.quit()
+    })
   }
 }
 
@@ -31,16 +32,18 @@ if (!shouldQuit) {
 } else {
   /*事件：second-instance 监听第二个实例是否已启动*/
   app.on("second-instance", (event, commandLine, workingDirectory) => {
-      // 当运行第二个实例时,将会聚焦到myWindow这个窗口
-      appStart.winRestoreFocus(win)
+    // 当运行第二个实例时,将会聚焦到myWindow这个窗口
+    appStart.winRestoreFocus(mainWin)
   })
-  /*事件：ready 当 Electron 完成初始化时，发出一次*/
-  app.on("ready", () => {
-      // protocolUtil.interceptFile()
-      // appStart.installDevtools()
-      createWindow()
-      appMenu.initMenu(win)
-      // ipcMain.register(win)
+  //只有ready之后才可以初始化窗体
+  app.whenReady().then(() => {
+    createWindow()
+    /*activate: macOS 当应用被激活时发出。*/
+    app.on("activate", () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    })
   })
 }
 /*window-all-closed:当所有窗体关闭时，退出应用*/
@@ -48,21 +51,14 @@ app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
-      app.quit()
+    app.quit()
   }
 })
-/*activate: macOS 当应用被激活时发出。*/
-app.on("activate", () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  // if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  if (win == null) {
-      createWindow()
-  }
-})
+
 
 
 function createWindow() {
-  win=appStart.initWindow()
-  appStart.initWinLoad(win)
+  mainWin = appStart.initWindow()
+  appStart.initWinLoad(mainWin)  
+  appMenu.initMenu(mainWin)
 }
