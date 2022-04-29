7892e8c2a256b184612692410e3f3e0fb6a8444b
pc-xuan\xuan <1633603801@qq.com>
2022-04-29 18:02:56 +0800

    关于中显示更多的版本号信息

--- electron/preload.js
+++ electron/preload.js
@@ -1,5 +1,4 @@
 // electron/preload.js
-
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
@@ -7,5 +6,8 @@ window.addEventListener('DOMContentLoaded', () => {
     console.debug(`${dependency}-version`, process.versions[dependency])
   }
 })
+// for (const dependency of ['chrome', 'node', 'electron']) {
+//   log.d(`${dependency}-version`, process.versions[dependency])
+// }
 /*去掉控制台关于security的警告*/
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
