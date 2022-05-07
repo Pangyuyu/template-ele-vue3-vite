ae5e3772fb63c7ec5444e582634d93afa3670fb7
pc-xuan\xuan <1633603801@qq.com>
2022-05-07 17:42:41 +0800

    进度条注意事项

--- src/view/techExample/ex_electron.vue
+++ src/view/techExample/ex_electron.vue
@@ -66,8 +66,8 @@
                 <el-button type="primary" @click="onClickNotifyMain()">主进程显示通知</el-button>
             </div>
             <div class="panel-warn">
-                <div class="item">1.渲染进程使用Notification完成通知U+002c此方式需要检验通知授权情况；详情参见：<a href="javascript:void(0)" @click="onClickOpenWindowByUrl('https://developer.mozilla.org/zh-CN/docs/Web/API/notification')">notification</a></div>
-                <div class="item">2.electron-notification-stateU+002c此模块可以检测是否允许发送通知；</div>
+                <div class="item">1.渲染进程使用Notification完成通知U+002c此方式需要检验通知授权情况U+003b详情参见：<a href="javascript:void(0)" @click="onClickOpenWindowByUrl('https://developer.mozilla.org/zh-CN/docs/Web/API/notification')">notification</a></div>
+                <div class="item">2.electron-notification-stateU+002c此模块可以检测是否允许发送通知U+003b</div>
                 <div class="item">3.windows上合理设置app.setAppUserModelId</div>
                 <div class="item">4.操作系统对通知正文字数有限制U+002c详情参见：<a href="javascript:void(0)" @click="onClickOpenWindowByUrl('https://www.electronjs.org/zh/docs/latest/tutorial/notifications')">通知（Notifications）</a></div>
             </div>
@@ -82,6 +82,11 @@
                 <el-button type="primary" @click="onClickProgressStart()" style="width:200px">启动进度条</el-button>
                 <el-button type="warning" @click="onClickProgressCancel()" style="width:200px">取消进度条</el-button>
                 <el-button type="info" @click="onClickProgressUnkown()" style="width:200px">设置进度条为不确定</el-button>
+            </div>
+            <div class="panel-warn">
+                <div class="item">1.进度条的值在0~1之间；</div>
+                <div class="item">2.值为“-1”是取消进度条；</div>
+                <div class="item">3.值大于“1”显示一个不确定的状态；</div>
             </div>
         </el-tab-pane>
         <el-tab-pane name="ex_more">
