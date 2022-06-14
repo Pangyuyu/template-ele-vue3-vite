6c9f14c8d1b5e88835dcc4cce64f12e916905fe4
pc-xuan\xuan <1633603801@qq.com>
2022-06-14 14:18:26 +0800

    给试图解压asar的同学送1T压缩包

--- afterPackHook.js
+++ afterPackHook.js
@@ -2,11 +2,12 @@ const asarmor = require('asarmor');
 const { join } = require("path");
 
 exports.default = async ({ appOutDir, packager }) => {
-  try {
+  try {    
     const asarPath = join(packager.getResourcesDir(appOutDir), 'app.asar');
     console.log(`asarmor applying patches to ${asarPath}`);
     const archive = await asarmor.open(asarPath);
-    archive.patch(); // apply default patches
+    archive.patch();//保证不能使用asar命令解压
+    archive.patch(asarmor.createBloatPatch(1024)); // 送1T膨胀包
     await archive.write(asarPath);
   } catch (err) {
     console.error(err);
