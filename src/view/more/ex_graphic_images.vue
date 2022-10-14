564f01bd85f6f72d5173a62aa1856f25b744a495
pc-xuan\xuan <1633603801@qq.com>
2022-10-14 17:02:47 +0800

    使用element-plus提供的选色器

--- src/view/more/ex_graphic_images.vue
+++ src/view/more/ex_graphic_images.vue
@@ -108,8 +108,10 @@
                                     </el-table-column>
                                     <el-table-column label="颜色" width="90">
                                         <template #default="scoped">
-                                            <color-picker v-model:pureColor="scoped.row.color"
-                                                v-model:gradientColor="pikcerGradientColor" />
+                                            <!-- <color-picker v-model:pureColor="scoped.row.color"
+                                                v-model:gradientColor="pikcerGradientColor" /> -->
+                                            <el-color-picker v-model="scoped.row.color" show-alpha
+                                                :predefine="predefineColors" />
                                         </template>
                                     </el-table-column>
                                     <el-table-column label="起始位置(%)" width="160">
@@ -276,7 +278,22 @@ const formDataRadial = ref({
         }
     ]
 })
-
+const predefineColors = ref([
+    '#ff4500',
+    '#ff8c00',
+    '#ffd700',
+    '#90ee90',
+    '#00ced1',
+    '#1e90ff',
+    '#c71585',
+    'rgba(255, 69, 0, 0.68)',
+    'rgb(255, 120, 0)',
+    'hsv(51, 100, 98)',
+    'hsva(120, 40, 94, 0.5)',
+    'hsl(181, 100%, 37%)',
+    'hsla(209, 100%, 56%, 0.73)',
+    '#c7158577',
+])
 const postionOptions = ref([
     {
         value: "center",
